c680468ae8b054bd40be80b847aa5190e1a6ebdf
Louis Savoie <thelousavoie@gmail.com>
2022-04-25 10:32:41 -0400

    Set up react-router in App
    Sets up react-router in App.js, adds Info/Projects/Blog components as
    route elements to the rounter, and adds render/snapshot tests for App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.scss'
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Info from './components/Info/Info'
+import Projects from './components/Projects/Projects'
+import Blog from './components/Blog/Blog'
 
 function App () {
   return (
-    <div className='testdiv'>
-      This text should be green.
+    <div data-testid="app">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Info />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,20 @@
-test('Test test should pass', () => {
-  expect(true).toBeTruthy()
+import React from 'react'
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import renderer from 'react-test-renderer'
+import App from './App'
+
+afterEach(() => {
+  cleanup()
+})
+
+test('should render App', () => {
+  render(<App />)
+  const AppElement = screen.getByTestId('app')
+  expect(AppElement).toBeInTheDocument()
+})
+
+test('matches snapshot', () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
