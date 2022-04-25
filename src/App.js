a0e847fde4dcaaf1e7ec251650a8bf1683ba1627
Louis Savoie <thelousavoie@gmail.com>
2022-04-25 13:13:08 -0400

    Fetch info.json data
    Adds state variable `data` to App along with function `getData` to fetch
    data from info.json and set it to `data`. `data` is temporarily console
    logged to prevent compilation error of 'variable declared but never
    used'.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Info from './components/Info/Info'
@@ -7,6 +7,26 @@ import Projects from './components/Projects/Projects'
 import Blog from './components/Blog/Blog'
 
 function App () {
+  const [data, setData] = useState([])
+
+  const getData = () => {
+    fetch('info.json',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        }
+      })
+      .then(function (response) { return response.json() })
+      .then(function (responseJson) { setData(responseJson) })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  console.log(data)
+
   return (
     <div data-testid="app">
       <Router>
