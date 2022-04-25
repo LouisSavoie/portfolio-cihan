878a5222165cf3484a07ea9efcc1e6a361640220
Louis Savoie <thelousavoie@gmail.com>
2022-04-25 14:34:02 -0400

    Fetch blog data
    Adds state variable `blogData` to Blog along with function `getBlogData`
    to fetch data from a medium blog through the rss2json API. `getBlogData` is temporarily console logged to prevent compilation error of 'variable is declared but nver used'.

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BlogCard from '../BlogCard/BlogCard'
 
 const Blog = () => {
+  const rss2json = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40' + 'thelousavoie' // TODO: replace string with prop from info.json data
+  const [blogData, setBlogData] = useState([])
+
+  const getBlogData = () => {
+    fetch(rss2json)
+      .then(response => response.json())
+      .then(responseJson => { setBlogData(responseJson) })
+  }
+
+  useEffect(() => {
+    getBlogData()
+  }, [rss2json])
+
+  console.log(blogData)
+
   return (
     <div data-testid="blog">
       <BlogCard />
