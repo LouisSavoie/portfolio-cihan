9cd6e960d338f16615df4ba64ab0abe65fa08ec3
Louis Savoie <thelousavoie@gmail.com>
2022-04-29 17:33:23 -0400

    basic layout of InfoPanel component
    Establishes the basic layout of the InfoPanel component with Bootstrap
    elements/classes. SOme of the placehoder images in the public folder
    have been replace with images of specced sizes for visual testing.

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const InfoPanel = ({ data }) => {
   const displayButtons = (dataValue, text) => {
-    return dataValue !== '' && <Button size="lg" href={dataValue} target="_blank" rel="noopener noreferrer">{text}</Button>
+    return dataValue !== '' && <Button href={dataValue} target="_blank" rel="noopener noreferrer">{text}</Button>
   }
 
   return (
-    <Card data-testid="info-panel" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={data.photo} />
-      <Card.Body>
-        <Card.Title as="h1">{data.name}</Card.Title>
-        <Card.Title as="h3">{data.title}</Card.Title>
-        {displayButtons(data.resume, 'Resume')}
-        <Button size="lg" href={`mailto:${data.email}`}>Email</Button>
-        {displayButtons(data.linkedin, 'LinkedIn')}
-        {displayButtons(data.github, 'GitHub')}
-        {displayButtons(data.leetcode, 'LeetCode')}
-        {displayButtons(data.codewars, 'Codewars')}
-        <Card.Text>{data.about}</Card.Text>
-      </Card.Body>
-    </Card>
+    <Container data-testid="info-panel">
+      <Card>
+        <Row className="justify-content-center" xs="1" sm="1" md="2">
+          <Col xs xxl="4" className="pe-md-0">
+            <Card.Img src={data.photo} style={{ borderRadius: '1rem' }} />
+            <Card.Body className="d-none d-md-flex d-lg-none justify-content-center flex-wrap">
+              {displayButtons(data.resume, 'Resume')}
+              <Button href={`mailto:${data.email}`}>Email</Button>
+              {displayButtons(data.linkedin, 'LinkedIn')}
+              {displayButtons(data.github, 'GitHub')}
+              {displayButtons(data.leetcode, 'LeetCode')}
+              {displayButtons(data.codewars, 'Codewars')}
+            </Card.Body>
+          </Col>
+          <Col xs xxl="8" className="ps-md-0">
+            <Card.Body>
+              <Card.Title as="h1" className="text-center">{data.name}</Card.Title>
+              <Card.Title as="h3" className="text-center">{data.title}</Card.Title>
+              <div className="d-xs-flex d-sm-flex d-md-none d-lg-flex justify-content-center flex-wrap">
+                {displayButtons(data.resume, 'Resume')}
+                <Button href={`mailto:${data.email}`}>Email</Button>
+                {displayButtons(data.linkedin, 'LinkedIn')}
+                {displayButtons(data.github, 'GitHub')}
+                {displayButtons(data.leetcode, 'LeetCode')}
+                {displayButtons(data.codewars, 'Codewars')}
+              </div>
+              <Card.Text>{data.about}</Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
   )
 }
 
