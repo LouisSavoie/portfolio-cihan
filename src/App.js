e9c6c08a414d0af5f02748286d091cccf60585ca
Louis Savoie <thelousavoie@gmail.com>
2022-05-08 10:50:04 -0400

    add toplink
    Adds a toplink to App as a FontAwesome icon(svg) nested in a link tag
    that is displayed after user scrolls down by half of the viewport height.`

--- src/App.js
+++ src/App.js
@@ -6,8 +6,21 @@ import Info from './components/Info/Info'
 import Projects from './components/Projects/Projects'
 import Blog from './components/Blog/Blog'
 import Footer from './components/Footer/Footer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 function App () {
+  const toplink = document.querySelector('#toplink')
+  const vh = document.documentElement.clientHeight / 2
+
+  window.onscroll = function () {
+    if (document.body.scrollTop >= vh || document.documentElement.scrollTop >= vh) {
+      toplink.classList.remove('d-none')
+    } else {
+      toplink.classList.add('d-none')
+    }
+  }
+
   const [data, setData] = useState([])
 
   const getData = () => {
@@ -35,6 +48,7 @@ function App () {
           <Route path="/projects" element={<Projects projects={data.projects} />} />
           <Route path="/blog" element={<Blog blogName={data.blog} />} />
         </Routes>
+        <a id="toplink" className="d-none" href="#"><FontAwesomeIcon icon={faArrowUp} /></a>
         <Footer />
       </Router>
     </div>
